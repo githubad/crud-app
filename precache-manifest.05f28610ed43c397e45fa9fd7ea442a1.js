93051ce5b8ed5465be2c672822b52d295a7430fb
Adnan Qaizar <adnanqaizar@gmail.com>
2018-11-28 14:17:20 +0500

    Updates

---
+++ precache-manifest.05f28610ed43c397e45fa9fd7ea442a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9aec0977b946066a4fe",
+    "url": "/crud-app/static/js/runtime~main.b9aec097.js"
+  },
+  {
+    "revision": "ecb13b28f3fff4cdb085",
+    "url": "/crud-app/static/js/main.ecb13b28.chunk.js"
+  },
+  {
+    "revision": "8b7cc0383b2404375f1c",
+    "url": "/crud-app/static/js/1.8b7cc038.chunk.js"
+  },
+  {
+    "revision": "ecb13b28f3fff4cdb085",
+    "url": "/crud-app/static/css/main.7c86eb04.chunk.css"
+  },
+  {
+    "revision": "f834c42806dfa60354057cc2ee181efd",
+    "url": "/crud-app/index.html"
+  }
+];
