a937fedc91e52cb9d62ba95711c3f17b96bda98f
Adnan Qaizar <adnanqaizar@gmail.com>
2018-12-01 00:13:14 +0500

    Updates

--- precache-manifest.33bb7de5bdce6abf24b3c8cf4df58a06.js
+++ precache-manifest.33bb7de5bdce6abf24b3c8cf4df58a06.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/crud-app/static/js/runtime~main.b9aec097.js"
   },
   {
-    "revision": "9abd1939f688880c9aeb",
-    "url": "/crud-app/static/js/main.9abd1939.chunk.js"
+    "revision": "f7ba4eb2a33f1ff7c2ad",
+    "url": "/crud-app/static/js/main.f7ba4eb2.chunk.js"
   },
   {
     "revision": "8b7cc0383b2404375f1c",
     "url": "/crud-app/static/js/1.8b7cc038.chunk.js"
   },
   {
-    "revision": "9abd1939f688880c9aeb",
+    "revision": "f7ba4eb2a33f1ff7c2ad",
     "url": "/crud-app/static/css/main.7c86eb04.chunk.css"
   },
   {
-    "revision": "14f3e684588fde3d6d2765f2f7308821",
+    "revision": "4de54c89278aa6c2cba1408f43ccd1c4",
     "url": "/crud-app/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/crud-app/precache-manifest.83a6a0ce468b56cf169f73f59f94e681.js"
+  "/crud-app/precache-manifest.33bb7de5bdce6abf24b3c8cf4df58a06.js"
 );
 
 workbox.clientsClaim();
